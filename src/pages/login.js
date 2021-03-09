63c874b8e5c9a2cd827fadbd38a10e8558615fe0
meymeynard <meymeynard@gmail.com>
2021-03-10 00:00:19 +0800

    chore: initial use of tailwind

--- src/pages/login.js
+++ src/pages/login.js
@@ -17,5 +17,11 @@ export default function Login() {
     document.title = 'Login - Instagram'
   }, [])
 
-  return <p>I am the login page</p>
+  return (
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+      <div className="flex w-3/5">
+        <img src="/images/iphone-with-profile.jpg" alt="iPhone with Instagram" />
+      </div>
+    </div>
+  )
 }
